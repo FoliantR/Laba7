4eecce273a92cfc8b3b1423deaad7e04fa9351d9
FoliantR <FoliantR@users.noreply.github.com>
2020-05-20 17:43:11 +0300

    Сделано

---
+++ Laba7/Laba7/MyClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba7
+{
+    class MyClassEnumer : IEnumerator, IEnumerable
+    {
+        string[] name;
+        int position = -1;
+        public MyClassEnumer(string[] _name)
+        {
+            this.name = _name;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (position == -1 || position >= name.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+                return name[position];
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return name.GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            if (position < name.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Laba7/Laba7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "Витя", "Алёша", "Мира", "Андрей", 
+                "Виктория", "Олег", "Кира" };
+            MyClassEnumer names = new MyClassEnumer(name);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
